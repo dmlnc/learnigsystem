7b9f00d3af811189be9540e778eeabea9bcc5221
Dmitrii <36060841+dmlnc@users.noreply.github.com>
2023-03-02 14:31:36 +0800

    upd

---
+++ laravel/resources/js/main.js
@@ -0,0 +1,53 @@
+/*
+=========================================================
+Muse - Vue Ant Design Dashboard - v1.0.0
+=========================================================
+
+Product Page: https://www.creative-tim.com/product/vue-ant-design-dashboard
+Copyright 2021 Creative Tim (https://www.creative-tim.com)
+Coded by Creative Tim
+
+=========================================================
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+*/
+
+import Antd from 'ant-design-vue';
+
+import './scss/app.scss';
+import 'ant-design-vue/dist/antd.css';
+
+import { createApp, h } from 'vue';
+import App from './App.vue'
+
+import DefaultLayout from './layouts/Default.vue'
+import DashboardLayout from './layouts/Dashboard.vue'
+import EmptyLayout from './layouts/Empty.vue'
+
+// import DashboardRTLLayout from './layouts/DashboardRTL.vue'
+import router from './router'
+// import './plugins/click-away'
+
+
+
+const app  = createApp({
+    render: ()=>h(App)
+});
+app.use(router);
+app.use(Antd);
+
+
+
+
+
+// Vue.config.productionTip = false
+
+// Adding template layouts to the vue components.
+app.component('layout-default', DefaultLayout);
+app.component('layout-dashboard', DashboardLayout);
+app.component('layout-empty', EmptyLayout);
+
+
+
+
+app.mount('#app');
+
