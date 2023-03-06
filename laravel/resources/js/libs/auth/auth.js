80858890f29cf71f8c387a9b3f9ba058c41ba6e4
Dmitrii <36060841+dmlnc@users.noreply.github.com>
2023-03-06 16:19:51 +0800

    Update auth.js

--- laravel/resources/js/libs/auth/auth.js
+++ laravel/resources/js/libs/auth/auth.js
@@ -1,21 +1,27 @@
 import axios from './axios';
 
-export function setAuthToken(token) {
-   if (token) {
-    // Apply authorization token to every request if logged in
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    // Store token in local storage
-    localStorage.setItem('access_token', token);
-  } else {
-    // Delete auth header
-    delete axios.defaults.headers.common['Authorization'];
-    // Remove token from local storage
-    localStorage.removeItem('access_token');
-  }
-}
+const authFunctions = {
 
 
-export function getAuthToken() {
-  const isAuthenticated = localStorage.getItem('access_token');
-  return isAuthenticated;
+    setAuthToken(token) {
+        if (token) {
+            // Apply authorization token to every request if logged in
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            // Store token in local storage
+            localStorage.setItem('access_token', token);
+        } else {
+            // Delete auth header
+            delete axios.defaults.headers.common['Authorization'];
+            // Remove token from local storage
+            localStorage.removeItem('access_token');
+        }
+    }
+
+
+    getAuthToken() {
+        const isAuthenticated = localStorage.getItem('access_token');
+        return isAuthenticated;
+    }
+
 }
+export default authFunctions
